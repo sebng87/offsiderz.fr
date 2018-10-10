3be631b9d563d31e6446a35eb4e968ede6914028
Sebastien Nguyen <fr.snguyen@gmail.com>
2018-10-10 10:01:47 +0100

    Add meta tags & GA script

--- templates/head.php
+++ templates/head.php
@@ -3,10 +3,43 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 
+    <meta name="description" content="« Ça va bien s’passer », un titre porteur d’espoir pour le 3ème album de Flynt qui avait mis tout le monde d’accord dès son premier opus..." />
+    <meta property="og:image" content="<?= get_template_directory_uri(); ?>/dist/images/common/social-share.jpg" />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="630" />
+    <meta property="og:locale" content="fr_FR" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Offsiderz | Ça va bien s’passer, 3ème album de Flynt" />
+    <meta property="og:description" content="« Ça va bien s’passer », un titre porteur d’espoir pour le 3ème album de Flynt qui avait mis tout le monde d’accord dès son premier opus..." />
+    <meta property="og:url" content="https://offsiderz.fr/" />
+    <meta property="og:site_name" content="Offsiderz" />
+    <meta property="fb:app_id" content="295150941085846" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@FLYNTMC" />
+    <meta name="twitter:creator" content="@FLYNTMC" />
+    <meta name="twitter:title" content="Offsiderz | Ça va bien s’passer, 3ème album de Flynt" />
+    <meta name="twitter:description" content="« Ça va bien s’passer », un titre porteur d’espoir pour le 3ème album de Flynt qui avait mis tout le monde d’accord dès son premier opus..." />
+    <meta name="twitter:image" content="<?= get_template_directory_uri(); ?>/dist/images/common/social-share.jpg" />
+    <meta name="twitter:image:width" content="1200" />
+    <meta name="twitter:image:height" content="630" />
+    <link rel="canonical" href="https://offsiderz.fr/" />
+    <script type="application/ld+json">{"@context":"http:\/\/schema.org","@type":"WebSite","url":"https:\/\/offsiderz.fr\/","name":"Offsiderz","potentialAction":{"@type":"SearchAction","target":"https:\/\/offsiderz.fr\/search\/{search_term_string}","query-input":"required name=search_term_string"}}</script>
+    <script type="application/ld+json">{"@context":"http:\/\/schema.org","@type":"Organization","url":"https:\/\/offsiderz.fr\/","name":"Offsiderz"}</script>
+
     <link rel="icon" href="<?= get_template_directory_uri(); ?>/dist/images/common/favicon.png" sizes="32x32">
     <link rel="icon" href="<?= get_template_directory_uri(); ?>/dist/images/common/favicon.png" sizes="192x192">
     
     <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville:400i|Montserrat:400,700,800" rel="stylesheet" type="text/css">
-    
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-127212809-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-127212809-1');
+    </script>
+
     <?php wp_head(); ?>
 </head>
